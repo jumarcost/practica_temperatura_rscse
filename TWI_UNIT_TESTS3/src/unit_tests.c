908c77a45c715798434aed2bf5019bb7cc1dec16
jumarcost <100363740@alumnos.uc3m.es>
2021-10-30 11:36:53 +0200

    Code clean and refactoring

--- TWI_UNIT_TESTS3/src/unit_tests.c
+++ TWI_UNIT_TESTS3/src/unit_tests.c
@@ -1,7 +1,4 @@
 #include <conf_usart_serial.h>
-
-
-// Se incluyen las bibliotecas necesarias
 #include <asf.h>
 #include <twi_megarf.h>
 #include <ioport.h>
@@ -9,20 +6,20 @@
 #include <util/delay.h>
 
 
-const uint8_t conf_data[] = {0x60};
+const uint8_t config_data[] = {0x60};
 
 typedef struct temp_ctx_t{
-	uint8_t readData[2];
+	uint8_t read_data[2];
 } TEMP_CTX_T;
 
 
-void sensor_conf (void);
+void sensor_config (void);
 uint8_t* read_temperature (void);
 void twi_init (void);
 
 TEMP_CTX_T temp_ctx;
 
-// Definimos la variable de la temperatura sacada del sensor externo
+// Puntero para guardar la temperatura medida
 uint8_t *pData;
 
 
@@ -31,28 +28,28 @@ uint8_t *pData;
 #define TWI_SLAVE_ADDR       0x96
 #define SLAVE_MEM_ADDR      0x01
 #define SLAVE_MEM_ADDR_LENGTH   TWI_SLAVE_ONE_BYTE_SIZE
-#define DATA_LENGTH  sizeof(conf_data)
+#define DATA_LENGTH  sizeof(config_data)
 
 
 
-void sensor_conf (void){
-	/* configures the TWI configuration packet*/
+void sensor_config (void){
+	//Paquete para la configuración del sensor
 	twi_package_t packet = {
 		.addr[0] = (uint8_t) SLAVE_MEM_ADDR,
 		.addr_length = (uint8_t)SLAVE_MEM_ADDR_LENGTH,
 		.chip = TWI_SLAVE_ADDR,
-		.buffer = (void *)conf_data,
+		.buffer = (void *)config_data,
 		.length = DATA_LENGTH
 	};
-	/* Perform a multi-byte write access */
+	//Escribe la configuración en el sensor de temperatura
 	while (twi_master_write(TWI_MASTER,&packet) != TWI_SUCCESS) {
 	}
-	/* waits for write completion*/
+	//Pequeño delay para que el sensor aploque la configuración
 	delay_ms(5);
 }
 
 
-// FUNCIÓN PARA LEER LA TEMPERATURA DEL SENSOR EXTERNO
+// Lectura de temperatura
 uint8_t* read_temperature (void){
 	uint8_t received_data[2] = {0, 0};
 		
@@ -68,30 +65,30 @@ uint8_t* read_temperature (void){
 	/* Perform a multi-byte read access*/
 	while (twi_master_read(TWI_MASTER,&packet_received) != TWI_SUCCESS) {
 	}
-	temp_ctx.readData[0] = received_data[0];
-	temp_ctx.readData[1] = received_data[1];
+	temp_ctx.read_data[0] = received_data[0];
+	temp_ctx.read_data[1] = received_data[1];
 	
-	// SE PUEDE IMPRIMIR POR EL HIPERTERMINAL AQUÍ, PERO LO HAREMOS EN EL MAIN
-	return temp_ctx.readData;
+	return temp_ctx.read_data;
 }
 
 
 
 void twi_init (void){
-	/* TWI master initialization options. */
+	//Configuración del TWI: Velocidad y direcciçon
 	twi_master_options_t m_options = {
 		.speed      = TWI_SPEED,
 		.chip  = TWI_SLAVE_ADDR,
 	};
 	m_options.baud_reg = TWI_CLOCK_RATE(sysclk_get_cpu_hz(), m_options.speed);
-	/* Enable the peripheral clock for TWI module */
+	//Habilita el reloj del periférico TWI
 	sysclk_enable_peripheral_clock(TWI_MASTER);
-	/* Initialize the TWI master driver. */
+	//Inicializa el TWI con la biblioteca
 	twi_master_init(TWI_MASTER,&m_options);
 }
 
 
 int main(void){
+	//Configuración de la USART como serie
 	const usart_serial_options_t usart_serial_options = {
 		.baudrate   = USART_SERIAL_BAUDRATE,
 		.charlength = USART_SERIAL_CHAR_LENGTH,
@@ -103,27 +100,24 @@ int main(void){
 	sysclk_init();
 	
 	ioport_init();
-	
+	//Asifnaión de la USART para las funciones de stdio como printf
 	stdio_serial_init(USART_SERIAL, &usart_serial_options);
 	twi_init();
-	sensor_conf();
+	sensor_config();
 	
 	
 	while(1) {
 	
-		// LEEMOS LA TEMPERATURA DEL SENSOR EXTERNO
+		// Lectura de temperatura
 		pData = read_temperature();
 	
 		// LA IMPRIMIMOS
-		printf("\n La temperatura es: %d,%d C \n \r",
-		pData[0],pData[1]);
-	
+		printf("\n La temperatura es de: %d,%d ºC \n \r",pData[0],pData[1]);
 		printf ("\n\r");
 	
-		delay_ms(1000); // Esperamos 1 segundo entre cada impresión
+		delay_ms(1000); // Delay de 1 segundo entre cada lectura
 	
-	} // FIN WHILE
-	
+	} 	
 	return 0;
 
 }
